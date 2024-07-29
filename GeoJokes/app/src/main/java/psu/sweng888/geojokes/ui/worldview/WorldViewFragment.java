e5fb362ce9e78afa91b6dde615f0c7591799f044
Braxton <134088619+Braxton-Evans@users.noreply.github.com>
2024-07-28 21:50:18 -0500

    Added FAB to reposition to jokes

--- GeoJokes/app/src/main/java/psu/sweng888/geojokes/ui/worldview/WorldViewFragment.java
+++ GeoJokes/app/src/main/java/psu/sweng888/geojokes/ui/worldview/WorldViewFragment.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 // Import local classes
@@ -31,6 +32,8 @@ public class WorldViewFragment extends Fragment implements OnMapReadyCallback {
     // Instance variables
     private FragmentWorldViewBinding binding;
     private final GeoJokes geoJokes = new GeoJokes();
+    private int JokeIdx = 0;
+    private GoogleMap mMap;
 
     // Fragment lifecycle method
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -45,6 +48,9 @@ public class WorldViewFragment extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFrag = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         assert mapFrag != null;
         mapFrag.getMapAsync(this);
+
+        // Bind the click listener for the FloatingActionButton
+        binding.floatingActionButton.setOnClickListener(ShowNextJoke());
 
         return binding.getRoot();
     }
@@ -66,15 +72,42 @@ public class WorldViewFragment extends Fragment implements OnMapReadyCallback {
      */
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        googleMap.setOnMapLoadedCallback(() -> {
+        mMap = googleMap;
+        mMap.setOnMapLoadedCallback(() -> {
             // Add markers for each GeoJoke
             for (GeoJokes.GeoJoke joke : geoJokes.list) {
-                googleMap.addMarker(
+                mMap.addMarker(
                     new MarkerOptions().position(joke.getLocation()).title(joke.getIntro()));
             }
             // Position the camera on the last GeoJoke
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                     geoJokes.list.get(geoJokes.list.size() - 1).getLocation(), 16));
         });
+    }
+
+    // Onclick for the FloatingActionButton
+    public View.OnClickListener ShowNextJoke() {
+        return view -> {
+            // Reposition the camera on the next GeoJoke
+            // Assuming that mMap is not null
+            if (mMap != null) {
+                if (JokeIdx >= geoJokes.list.size()) {
+                    // Reset the index if we've reached the end
+                    JokeIdx = 0;
+                }
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        geoJokes.list.get(JokeIdx++).getLocation(), 10));
+            }
+        };
+    }
+
+    // Onclick for markers on the map
+    public GoogleMap.OnMarkerClickListener MarkerClicked(Marker marker) {
+        return view -> {
+            // Toggle the marker's title between the intro and punchline
+            if (marker.getTitle().equals(geoJokes.list.get(JokeIdx).getIntro())) {
+                marker.setTitle(geoJokes.list.get(JokeIdx).getPunchline());
+            }
+        };
     }
 }
